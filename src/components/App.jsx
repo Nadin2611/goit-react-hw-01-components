286f365dfa24ea4c822a2a1ba5e742c0ccaf2c92
Nadin2611 <kyrychenko.nadia07@gmail.com>
2023-11-06 10:22:53 +0200

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { Profile } from './Profile/Profile';
+import userData from '../data/user.json';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        fontWeight: 'bold',
+        gap: '40px',
       }}
     >
       React homework template
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
     </div>
   );
 };
---
+++ src/components/Profile/Profile.styled.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+
+export const ProfileContainer = styled.div`
+  background: ${props => props.theme.colors.white};
+  padding-top: ${props => props.theme.spacing(10)};
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 250px;
+  height: 270px;
+  width: 100%;
+  margin: 0 auto;
+  overflow: hidden;
+  box-shadow: ${props => props.theme.shadows.regular};
+`;
+
+export const DescriptionContainer = styled.div`
+  text-align: center;
+`;
+
+export const Avatar = styled.img`
+  width: ${props => props.theme.spacing(20)};
+  height: ${props => props.theme.spacing(20)};
+  border-radius: 50%;
+  border: 2px solid ${props => props.theme.colors.gray};
+  padding: ${props => props.theme.spacing(1)};
+  margin: 0 auto;
+`;
+
+export const Name = styled.p`
+  font-size: ${props => props.theme.fontSizes.large};
+  font-weight: bold;
+  margin-top: ${props => props.theme.spacing(4)};
+  margin-bottom: ${props => props.theme.spacing(2)};
+`;
+
+export const Tag = styled.p`
+  font-size: ${props => props.theme.fontSizes.xs};
+  color: ${props => props.theme.colors.gray};
+`;
+
+export const Location = styled.p`
+  font-size: ${props => props.theme.fontSizes.xs};
+  color: ${props => props.theme.colors.gray};
+`;
+
+export const StatsList = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin-bottom: 0;
+  margin-top: auto;
+`;
+
+export const StatsItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  justify-content: center;
+  background-color: ${props => props.theme.colors.greyBlue};
+  height: 60px;
+  font-size: ${props => props.theme.fontSizes.xs};
+  flex: 1;
+  align-items: center;
+  &:not(:last-child) {
+    border-right: 1px solid #bec9ed;
+  }
+`;
+
+export const Label = styled.span`
+  color: ${props => props.theme.colors.gray};
+`;
+
+export const Quantity = styled.span`
+  color: ${props => props.theme.colors.dark};
+  font-weight: bold;
+`;
